a0d78d99b5ddc7a3fa831b28873433708a461fea
JuanGR-FI <juan60430@hotmail.com>
2023-09-30 14:07:30 -0600

    Adding Key configuration

---
+++ Marvel/Extension/DoubleExtension.swift
@@ -0,0 +1,8 @@
+//
+//  DoubleExtension.swift
+//  Marvel
+//
+//  Created by Juan Andrés Cervantes Guati Rojo on 30/09/23.
+//
+
+import Foundation
---
+++ Marvel/Model/Keys.swift
@@ -0,0 +1,8 @@
+//
+//  Keys.swift
+//  Marvel
+//
+//  Created by Juan Andrés Cervantes Guati Rojo on 30/09/23.
+//
+
+import Foundation
